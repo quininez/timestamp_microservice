715a3deae459cf80d9a985dcd929f687811b94ee
Vali <quinine35@gmail.com>
2016-05-11 13:29:19 +0000

    added all files

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var moment = require("moment");
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+  res.end();
+});
+
+app.get('/:time', function(req,res){
+  var unixtime, date, naturaldate;
+  if (Number.isInteger(+req.params.time)){
+    unixtime = +req.params.time;
+    date = moment(unixtime * 1000);
+  } else {
+    date = moment(req.params.time);
+    unixtime = date.format('x');
+  }
+   
+  naturaldate = date.format('MMMM DD, YYYY');
+  
+  if (!date._isValid) {
+    unixtime = null;
+    naturaldate = null;
+  }
+  
+  var result = {
+    unix: unixtime,
+    natural: naturaldate
+  }
+  res.end(JSON.stringify(result));
+});
+
+app.listen(process.env.PORT || 8080, function(err,log){
+  if (err) throw err;
+  console.log('Server running on port: '+ process.env.PORT);
+});
